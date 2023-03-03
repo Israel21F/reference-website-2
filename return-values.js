bd7f4f1d26c813c1aae4c8971551343b2d123e67
Israel21F <126812093+Israel21F@users.noreply.github.com>
2023-03-03 10:21:23 -0700

    Add files via upload

---
+++ return-values.js
@@ -0,0 +1,27 @@
+// Start function for the example will run when the test button is clicked
+function example(){
+ var area1 = areaOfRect(20, 50);
+ var area2 = areaOfRect(40, 80);
+ alert(area1 + area2);
+}
+
+function areaOfRect(length, width){
+    var area = length * width;
+    return area; 
+}
+
+
+// Start function for problem 1
+function problem1() {
+ var num1 = isEven(20,40);
+ var num2 = isOdd(21,11);
+ alert(isEven);
+}
+
+
+
+
+// Start function for problem 2
+function problem2() {
+  
+}
